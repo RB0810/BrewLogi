87c2a93d961c3e6a8c63185860c91ab52ab65f55
RB0810 <rishika0810banka@gmail.com>
2023-05-16 14:38:47 +0800

    alert toast

--- app/src/main/java/com/example/brewlogi/AlertFragment.java
+++ app/src/main/java/com/example/brewlogi/AlertFragment.java
@@ -12,14 +12,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.onesignal.OSNotification;
 import com.onesignal.OSNotificationReceivedEvent;
 import com.onesignal.OneSignal;
 
+import java.util.ArrayList;
+
 public class AlertFragment extends Fragment {
     private TextView alert;
     private View rootview;
+    Integer alertNumber = 100;
+    ArrayList<String> alertText = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -45,6 +55,39 @@ public class AlertFragment extends Fragment {
 
             }
         });
+        DatabaseReference database = FirebaseDatabase.getInstance("https://hacksingapore-14b13-default-rtdb.asia-southeast1.firebasedatabase.app/")
+                .getReference("Inventory");
+        database.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    String stall = dataSnapshot.getKey();
+                    for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+                        String product = dataSnapshot1.getKey();
+                        DataSnapshot dataSnapshot2 = dataSnapshot1.child("Cans left");
+                        Integer cansLeft = dataSnapshot2.getValue(Integer.class);
+                        if (cansLeft < alertNumber) {
+                            String alert = stall + ", " + product + " is low on stock";
+                            if (!alertText.contains(alert)) {
+                                Toast.makeText(rootview.getContext(), alert, Toast.LENGTH_LONG).show();
+                                alertText.add(alert);
+                            }
+                        } else {
+                            String alert = stall + ", " + product + " is low on stock";
+                            if (alertText.contains(alert)) {
+                                alertText.remove(alert);
+                            }
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
         return rootview;
     }
 
