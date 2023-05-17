906c2c34e0dcb944dcbe34f44f4d2947fc53384b
RB0810 <rishika0810banka@gmail.com>
2023-05-17 18:08:26 +0800

    organiser verification

--- app/src/main/java/com/example/brewlogi/LoginOrganiser.java
+++ app/src/main/java/com/example/brewlogi/LoginOrganiser.java
@@ -2,6 +2,7 @@ package com.example.brewlogi;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -25,26 +26,35 @@ public class LoginOrganiser extends AppCompatActivity {
         passwordget = findViewById(R.id.password);
         Button loginButton = findViewById(R.id.loginbutton);
 
-        loginButton.setOnClickListener(v -> {
-            String email = emailget.getText().toString();
-            String password = passwordget.getText().toString();
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailget.getText().toString();
+                String password = passwordget.getText().toString();
 
-            FirebaseAuth auth = FirebaseAuth.getInstance();
+                FirebaseAuth auth = FirebaseAuth.getInstance();
+                if (email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(LoginOrganiser.this, "Please fill in all fields", Toast.LENGTH_LONG).show();
+                } else {
+                    auth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(task -> {
+                                if (task.isSuccessful()) {
+                                    // Authentication successful
+                                    FirebaseUser user = auth.getCurrentUser();
+                                    Intent intent = new Intent(LoginOrganiser.this, Organiser.class);
+                                    startActivity(intent);
+                                } else {
+                                    // Authentication failed
+                                    Exception exception = task.getException();
+                                    Toast.makeText(LoginOrganiser.this, "Invalid id or password", Toast.LENGTH_LONG).show();
+                                }
+                            });
+                }
+            }
 
-            auth.signInWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(task -> {
-                        if (task.isSuccessful()) {
-                            // Authentication successful
-                            FirebaseUser user = auth.getCurrentUser();
-                            Intent intent = new Intent(LoginOrganiser.this, Organiser.class);
-                            startActivity(intent);
-                        } else {
-                            // Authentication failed
-                            Exception exception = task.getException();
-                            Toast.makeText(this, "Invalid id or password", Toast.LENGTH_LONG).show();
-                        }
-                    });
         });
-    }
 
+    }
 }
+
+
